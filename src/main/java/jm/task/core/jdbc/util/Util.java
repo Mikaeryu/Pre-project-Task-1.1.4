187e0e421b44d3db97058077378d113edc0b88d8
Mike <hgelhem@gmail.com>
2023-02-22 14:00:29 +0300

    changed Util class, so it has one return and cleaned some code

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,12 +3,13 @@ package jm.task.core.jdbc.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Objects;
 
 public class Util {
     // реализуйте настройку соеденения с БД
 
     public static Connection getConnection() {
-        Connection connection;
+        Connection connection = null;
 
         String dbURL = "jdbc:mysql://127.0.0.1:3306/users?useSSL=false";
         String userName = "root";
@@ -17,16 +18,11 @@ public class Util {
         try {
             connection = DriverManager.getConnection(dbURL, userName, password);
 
-            if (connection == null) {
-                System.out.println("There is no connection with Database");
-            } //Shoud change this if???
-
-            return connection; // maybe delete this return line in try block and use one return at the bottom?
         } catch (SQLException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
 
-        return null; // change return here to requireNonNull return and delete requireNonNull options in Impl class
+        return Objects.requireNonNull(connection, "There is no connection with Database");
     }
 }

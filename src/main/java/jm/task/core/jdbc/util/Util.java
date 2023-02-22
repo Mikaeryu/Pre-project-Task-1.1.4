41ba0d732de2d87b2798042f593a3ad7e335641c
Mike <hgelhem@gmail.com>
2023-02-22 14:14:00 +0300

    polished Util class so it throws SQLException

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -8,19 +8,14 @@ import java.util.Objects;
 public class Util {
     // реализуйте настройку соеденения с БД
 
-    public static Connection getConnection() {
+    public static Connection getConnection() throws SQLException{
         Connection connection = null;
 
         String dbURL = "jdbc:mysql://localhost:3306/users?useSSL=false";
         String userName = "root";
         String password = "Ch3ck4dGR4Y!";
 
-        try {
-            connection = DriverManager.getConnection(dbURL, userName, password);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-        }
+        connection = DriverManager.getConnection(dbURL, userName, password);
 
         return Objects.requireNonNull(connection, "There is no connection with Database");
     }

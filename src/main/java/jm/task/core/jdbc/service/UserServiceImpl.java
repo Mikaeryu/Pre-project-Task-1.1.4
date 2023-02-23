5d7b24161e1b936dd635f18c060ad7288492ca82
Mike <hgelhem@gmail.com>
2023-02-23 11:12:31 +0300

    changed USER_DAO_JDBC to USER_DAO

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,40 +1,41 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private final static UserDaoJDBCImpl USER_DAO_JDBC = new UserDaoJDBCImpl();
+    private static final UserDao USER_DAO = new UserDaoJDBCImpl();
 
     @Override
     public void createUsersTable() {
-        USER_DAO_JDBC.createUsersTable();
+        USER_DAO.createUsersTable();
     }
 
     @Override
     public void dropUsersTable() {
-        USER_DAO_JDBC.dropUsersTable();
+        USER_DAO.dropUsersTable();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        USER_DAO_JDBC.saveUser(name, lastName, age);
+        USER_DAO.saveUser(name, lastName, age);
     }
 
     @Override
     public void removeUserById(long id) {
-        USER_DAO_JDBC.removeUserById(id);
+        USER_DAO.removeUserById(id);
     }
 
     @Override
     public List<User> getAllUsers() {
-        return USER_DAO_JDBC.getAllUsers();
+        return USER_DAO.getAllUsers();
     }
 
     @Override
     public void cleanUsersTable() {
-        USER_DAO_JDBC.cleanUsersTable();
+        USER_DAO.cleanUsersTable();
     }
 }

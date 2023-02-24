e1f71124a138ec04d9414f530ae093fa5f6aa77e
Mike <hgelhem@gmail.com>
2023-02-24 19:19:40 +0300

    changed Util class, so it has constant fields

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,17 +1,33 @@
 package jm.task.core.jdbc.util;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
     // реализуйте настройку соеденения с БД
+    private Util() {}
+
+    private static SessionFactory factory;
+    private static ServiceRegistry serviceRegistry;
+
+    static final String DB_URL = "jdbc:mysql://localhost:3306/users_db?useSSL=false";
+    static final String USER_NAME = "root";
+    static final String PASSWORD = "Ch3ck4dGR4Y!";
 
     public static Connection getConnection() throws SQLException{
-        String dbURL = "jdbc:mysql://localhost:3306/users_db?useSSL=false";
-        String userName = "root";
-        String password = "Ch3ck4dGR4Y!";
+        return DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+    }
 
-        return DriverManager.getConnection(dbURL, userName, password);
+    public static Session getSession() {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        return null;
     }
 }

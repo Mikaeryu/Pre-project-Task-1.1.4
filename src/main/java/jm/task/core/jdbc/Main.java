1cb116a475d0319b82341995482763d6950f3364
Mike <hgelhem@gmail.com>
2023-02-27 22:06:30 +0300

    reordered imports in Main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,10 +1,10 @@
 package jm.task.core.jdbc;
 
+import java.util.List;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {

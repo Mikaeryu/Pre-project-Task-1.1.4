50896e715856e64490817f15f0bdcac6291e9d25
Mike <hgelhem@gmail.com>
2023-02-27 09:24:23 +0300

    changed tests in Main a bit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -22,7 +22,7 @@ public class Main {
         userService.saveUser("Vasya", "Pupkin", (byte) 0);
 
         List<User> userList = userService.getAllUsers();
-        System.out.println(userList);
+        userList.forEach(System.out::println);
 
         Util.closeSessionFactory();
 
